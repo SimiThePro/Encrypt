66e1f3fe3e829bace6d50bab4b769e9ea7d6fa69
SimiThePro <61706852+SimiThePro@users.noreply.github.com>
2022-12-01 16:41:53 +0100

    Update Caesar_Verschluesselung.cpp
    Added QOL Features, Working On Brute Force

--- Caesar_Verschluesselung/Caesar_Verschluesselung.cpp
+++ Caesar_Verschluesselung/Caesar_Verschluesselung.cpp
@@ -4,6 +4,7 @@
 #include <sstream>
 #include <Windows.h>
 #include <conio.h>
+#include <vector>
 
 
 using namespace std;
@@ -14,18 +15,12 @@ string path = "Dateien/";
 #define SHOWDIR system(command)
 #define CLEAR system("cls")
 
-void Menu();
-void Verschluesseln();
-void Entschluesseln();
-
-
 struct Encrypt {
-public:
+public: 
     int key = 0;
     string DateinameZuVer = "";
     string DateinameVer = "";
-    ifstream DateiZuVer;
-    ofstream DateiVer;
+    
 };
 struct Decrypt
 {
@@ -33,78 +28,42 @@ public:
     int key = 0;
     string DateinameZuEnt = "";
     string DateinameEnt = "";
-    ifstream DateiZuEnt;
-    ofstream DateiEnt;
 };
 
+void Menu();
+void Verschluesseln(struct Encrypt &encrypt);
+void Entschluesseln(struct Decrypt &decrypt);
 
 int main()
 {
-    Menu();
+    struct Encrypt encrypt2;
+    struct Decrypt decrypt;
     string Eingabe, FileInput;
-    int iEingabe;
-    cout << "\n>>";
-    getline(cin, Eingabe);
-    stringstream(Eingabe) >> iEingabe;
-    
-    while (iEingabe != 3)
-    switch (iEingabe) {
-    case 1:
-        Verschluesseln();
-        break;
-    case 2:
-        Entschluesseln();
-        break;
-    }
+    int iEingabe = 0;
     
 
-    ofstream MyFile(path + "Informationen.txt");
-    MyFile << Eingabe;
-    MyFile.close();
-
-    /*ifstream MyReadFile(path +"Informationen.txt");
-    if (MyReadFile.is_open()) {
-        while (getline(MyReadFile, FileInput)) {
+    while (iEingabe != 4)
+    {
+        CLEAR;
+        Menu();
+        cout << "\n>>";
+        getline(cin, Eingabe);
+        stringstream(Eingabe) >> iEingabe;
+        switch (iEingabe) {
+        case 1:
+            Verschluesseln(encrypt2);
+            break;
+        case 2:
+            Entschluesseln(decrypt);
+            break;
+        case 3: {
+            Bruteforce();
+        }
+        case 4:
+            break;
 
         }
-    }*/
-    Eingabe = FileInput;
-    string lowerEingabe = FileInput;
-
-    for (int i = 0, deleted = 0; i < Eingabe.length(); i++) {
-        if (isupper(Eingabe[i]) && isalpha(Eingabe[i])) {
-            lowerEingabe[i] = Eingabe[i] + 32;
-        }
-        else if (islower(Eingabe[i]))lowerEingabe[i] = Eingabe[i];
     }
-
-    int schluessel;
-    cin >> schluessel;
-    //Encrypt//
-    for (int i = 0; i < lowerEingabe.length(); i++) {
-        if (lowerEingabe[i] < 'z' - schluessel)
-            lowerEingabe[i] += schluessel;
-        else lowerEingabe[i] = (lowerEingabe[i] % ('z' - schluessel)) + 'a' - 1;
-    }
-    cout << lowerEingabe << endl;
-
-    //Decrypt//
-    for (int i = 0; i < lowerEingabe.length(); i++) {
-        if (lowerEingabe[i] > 'a' + schluessel - 1)
-        {
-            lowerEingabe[i] -= schluessel;
-        }
-        else lowerEingabe[i] = 'z' - (schluessel - (lowerEingabe[i] % ('a')) - 1);
-    }
-
-    cout << lowerEingabe << endl;
-
-
-
-    //Show Directory//
-    system(command);
-    
-
 }
 
 
@@ -115,42 +74,46 @@ void Menu() {
         "\na) Wahl des Schluessels"
         "\nb) Wahl der zu verschluesselnden Datei"
         "\nc) Wahl der verschluesselten Datei"
-        "\n2.	Entschlüsseln"
+        "\n2.	Entschluesseln"
         "\na) Eingabe des Schluessels"
         "\nb) Wahl der verschluesselten Datei"
         "\nc) Wahl der entschluesselten Datei"
         "\n3.	Ende";
 }
 
-void Verschluesseln() {
+void Verschluesseln(struct Encrypt &encrypt) {
     //Deklaration
-    Encrypt encrypt;
+
+    ifstream DateiZuVer;
+    ofstream DateiVer;
+
     string strschluessel;
     string sWahl; int Wahl = 0;
     string line, FileText, LowerText = "", Eingabe;
 
-    while (Wahl != 4) {
+    while (Wahl != 5) {
         Wahl = 0;
-        int tempkey = 0;
         CLEAR;
         cout <<
             "\n 1) Wahl des Schluessels \t\t\t(Momentan: " << encrypt.key << ")" <<
             "\n 2) Wahl der zu verschluesselnden Datei \t" << (encrypt.DateinameZuVer == "" ? "KEINE AUSGEWAEHLT" : encrypt.DateinameZuVer + ".txt") <<
-            "\n 3) Wahl der verschluesselten Datei \t\t" << (encrypt.DateinameVer == "" ? "KEINE AUSGEWAEHLT" : encrypt.DateinameVer + ".txt")  <<
-            "\n 4) AUSFUEHREN";
+            "\n 3) Wahl der verschluesselten Datei \t\t" << (encrypt.DateinameVer == "" ? "KEINE AUSGEWAEHLT" : encrypt.DateinameVer + ".txt") <<
+            "\n 4) AUSFUEHREN" <<
+            "\n 5 Ende";
 
-                
-        while (Wahl < 1 || Wahl > 4)
+
+        while (Wahl < 1 || Wahl > 5)
         {
             cout << "\n>>";
             getline(cin, sWahl);
             stringstream(sWahl) >> Wahl;
-            if (Wahl < 1 || Wahl > 4) cout << "Please enter a valid Number";
+            if (Wahl < 1 || Wahl > 5) cout << "Please enter a valid Number";
         }
 
         switch (Wahl)
         {
         case 1: {
+            int tempkey = 0;
             cout << "Geben Sie den Schluessel ein: ";
             while (tempkey == 0) {
                 getline(cin, strschluessel);
@@ -160,19 +123,21 @@ void Verschluesseln() {
                 }
             }
             encrypt.key = tempkey;
+
             break;
+
         }
         case 2: {
             cout << "Welche der Folgenden Dateien moechten sie verschluesseln\n";
             SHOWDIR;
-            for (encrypt.DateiZuVer; !encrypt.DateiZuVer.is_open();)
+            for (DateiZuVer; !DateiZuVer.is_open();)
             {
                 getline(cin, encrypt.DateinameZuVer);
-                encrypt.DateiZuVer.open(path + encrypt.DateinameZuVer+ ".txt");
-                if (!encrypt.DateiZuVer.is_open())
+                DateiZuVer.open(path + encrypt.DateinameZuVer + ".txt");
+                if (!DateiZuVer.is_open())
                     cout << "unable to open file" << endl;
             }
-            while (getline(encrypt.DateiZuVer, line)) {
+            while (getline(DateiZuVer, line)) {
                 FileText += line + "\n";
             }
             cout << "Möchten Sie die Datei auslesen?   (j=ja|n=nein)  " << endl;
@@ -181,47 +146,41 @@ void Verschluesseln() {
                 cout << FileText;
                 system("pause");
             }
-            encrypt.DateiZuVer.close();
+            DateiZuVer.close();
             break;
         }
         case 3: {
             cout << "In Welcher Datei möchten Sie die verschlüsselung speichern\n";
             SHOWDIR;
-            for (encrypt.DateiVer; !encrypt.DateiVer.is_open();)
+            for (DateiVer; !DateiVer.is_open();)
             {
                 getline(cin, encrypt.DateinameVer);
-                encrypt.DateiVer.open(path + encrypt.DateinameVer + ".txt");
-                if (!encrypt.DateiVer.is_open())
+                DateiVer.open(path + encrypt.DateinameVer + ".txt");
+                if (!DateiVer.is_open())
                     cout << "unable to open file" << endl;
             }
-            encrypt.DateiVer.close();
+            DateiVer.close();
             break;
         }
         case 4: {
-            encrypt.DateiZuVer.open(path + encrypt.DateinameZuVer + ".txt");
+            DateiZuVer.open(path + encrypt.DateinameZuVer + ".txt");
             cout << "Um Fortzufahren drücken Sie ENTER";
             getline(cin, Eingabe);
             if (Eingabe == "") {
-                while (getline(encrypt.DateiZuVer,line)){
-                    /*for (int i = 0; i < line.length(); i++) {
-                        if (line [i] >= 'A' && line[i] <='Z') {
-                            line[i] = line[i] + 32;
-                        }
-                        else if (islower(line[i]))line[i] = line[i];
-                    }*/
+                while (getline(DateiZuVer, line)) {
                     LowerText += line + "\n";
                 }
-                encrypt.DateiZuVer.close();
+                DateiZuVer.close();
                 cout << endl << LowerText;
 
 
                 string encryptedtext(LowerText.length(), ' ');
 
-                for (int i = 0, endlines = 0; i < LowerText.length()-endlines; i++) {
+                for (int i = 0, endlines = 0; i < LowerText.length() - endlines; i++) {
                     if ((LowerText[i] >= 'A' && LowerText[i] <= 'Z') || (LowerText[i] >= 'a' && LowerText[i] <= 'z'))
                     {
                         if (LowerText[i] >= 'A' && LowerText[i] <= 'Z')
-                        { 
+                        {
                             if (LowerText[i] <= ('Z') - encrypt.key)
                                 encryptedtext[i] = (LowerText[i] + encrypt.key);
                             else encryptedtext[i] = ((LowerText[i] % ('Z' - encrypt.key)) + 'A' - 1);
@@ -232,46 +191,58 @@ void Verschluesseln() {
                                 encryptedtext[i] = (LowerText[i] + encrypt.key);
                             else encryptedtext[i] = ((LowerText[i] % ('z' - encrypt.key)) + 'a' - 1);
                         }
-                        
-                    }else if (LowerText[i] == '\n')
+
+                    }
+                    else if (LowerText[i] == '\n')
                     {
                         endlines++;
                         encryptedtext[i] = LowerText[i];
                     }
                     else encryptedtext[i] = LowerText[i] + encrypt.key;
-                    
+
                 }
-                encrypt.DateiVer.open(path + encrypt.DateinameVer + ".txt");
+                DateiVer.open(path + encrypt.DateinameVer + ".txt");
 
-                encrypt.DateiVer << encryptedtext;
+                DateiVer << encryptedtext;
 
-                encrypt.DateiVer.close();
+                DateiVer.close();
+
+                cout << "\nDie Datei wurde von der Datei \"" + encrypt.DateinameZuVer + "\".txt mit den " <<
+                    "Schluessel " << encrypt.key << " in die Datei \"" << encrypt.DateinameVer << ".txt verschluesselt." << endl;
+                system("pause");
 
             }
-            
+
+        }
+        case 5: {
+
+            break;
         }
         }
     }
 }
 
-void Entschluesseln() {
-    Decrypt decrypt;
+void Entschluesseln(struct Decrypt &decrypt) {
+
+    ifstream DateiZuEnt;
+    ofstream DateiEnt;
     string strschluessel;
     string sWahl; int Wahl = 0;
     string line, FileText, LowerText = "", Eingabe;
 
-    while (Wahl != 4) {
+    while (Wahl != 5) {
         Wahl = 0;
-        int tempkey = 0;
+        
         CLEAR;
         cout <<
             "\n 1) Wahl des Schluessels \t\t\t(Momentan: " << decrypt.key << ")" <<
-            "\n 2) Wahl der verschlüsselten Datei \t" << (decrypt.DateinameZuEnt == "" ? "KEINE AUSGEWAEHLT" : decrypt.DateinameZuEnt + ".txt") <<
-            "\n 3) Wahl der entschlüsselten Datei \t" << (decrypt.DateinameEnt == "" ? "KEINE AUSGEWAEHLT" : decrypt.DateinameEnt + ".txt") <<
-            "\n 4) AUSFUEHREN";
+            "\n 2) Wahl der verschluesselten Datei \t" << (decrypt.DateinameZuEnt == "" ? "KEINE AUSGEWAEHLT" : decrypt.DateinameZuEnt + ".txt") <<
+            "\n 3) Wahl der entschluesselten Datei \t" << (decrypt.DateinameEnt == "" ? "KEINE AUSGEWAEHLT" : decrypt.DateinameEnt + ".txt") <<
+            "\n 4) AUSFUEHREN"
+            "\n 5) ENDE";
 
 
-        while (Wahl < 1 || Wahl > 4)
+        while (Wahl < 1 || Wahl > 5)
         {
             cout << "\n>>";
             getline(cin, sWahl);
@@ -282,6 +253,7 @@ void Entschluesseln() {
         switch (Wahl)
         {
         case 1: {
+            int tempkey = 0;
             cout << "Geben Sie den Schluessel ein: ";
             while (tempkey == 0) {
                 getline(cin, strschluessel);
@@ -296,14 +268,14 @@ void Entschluesseln() {
         case 2: {
             cout << "Welche der Folgenden Dateien moechten sie entschluesseln\n";
             SHOWDIR;
-            for (decrypt.DateiZuEnt; !decrypt.DateiZuEnt.is_open();)
+            for (DateiZuEnt; !DateiZuEnt.is_open();)
             {
                 getline(cin, decrypt.DateinameZuEnt);
-                decrypt.DateiZuEnt.open(path + decrypt.DateinameZuEnt + ".txt");
-                if (!decrypt.DateiZuEnt.is_open())
+                DateiZuEnt.open(path + decrypt.DateinameZuEnt + ".txt");
+                if (!DateiZuEnt.is_open())
                     cout << "unable to open file" << endl;
             }
-            while (getline(decrypt.DateiZuEnt, line)) {
+            while (getline(DateiZuEnt, line)) {
                 FileText += line + "\n";
             }
             cout << "Möchten Sie die Datei auslesen?   (j=ja|n=nein)  " << endl;
@@ -312,37 +284,38 @@ void Entschluesseln() {
                 cout << FileText;
                 system("pause");
             }
-            decrypt.DateiZuEnt.close();
+            DateiZuEnt.close();
             break;
         }
         case 3: {
             cout << "In Welcher Datei möchten Sie die entschlüsselung speichern\n";
             SHOWDIR;
-            for (decrypt.DateiEnt; !decrypt.DateiEnt.is_open();)
+            for (DateiEnt; !DateiEnt.is_open();)
             {
                 getline(cin, decrypt.DateinameEnt);
-                decrypt.DateiEnt.open(path + decrypt.DateinameEnt + ".txt");
-                if (!decrypt.DateiEnt.is_open())
+                DateiEnt.open(path + decrypt.DateinameEnt + ".txt");
+                if (!DateiEnt.is_open())
                     cout << "unable to open file" << endl;
             }
-            decrypt.DateiEnt.close();
+            DateiEnt.close();
             break;
         }
         case 4: {
-            decrypt.DateiZuEnt.open(path + decrypt.DateinameZuEnt + ".txt");
+            DateiZuEnt.open(path + decrypt.DateinameZuEnt + ".txt");
             cout << "Um Fortzufahren drücken Sie ENTER";
             getline(cin, Eingabe);
             if (Eingabe == "") {
+                Wahl = 4;
                 string encryptedtext = "", line;
-                while (getline(decrypt.DateiZuEnt, line))
+                while (getline(DateiZuEnt, line))
                     encryptedtext += line + '\n';
 
                 string decryptedtext(encryptedtext.length(), ' ');
 
                 for (int i = 0, endlines = 0; i < encryptedtext.length() - endlines; i++) {
-                    
-                    if ((encryptedtext[i] >= 'a' && encryptedtext[i] <= 'z')||(encryptedtext[i] >= 'A' && encryptedtext[i] <= 'Z')) {
-                        
+
+                    if ((encryptedtext[i] >= 'a' && encryptedtext[i] <= 'z') || (encryptedtext[i] >= 'A' && encryptedtext[i] <= 'Z')) {
+
                         if (encryptedtext[i] >= 'a' && encryptedtext[i] <= 'z')
                         {
                             if (encryptedtext[i] >= ('a') + decrypt.key)
@@ -354,7 +327,7 @@ void Entschluesseln() {
                                 decryptedtext[i] = (encryptedtext[i] - decrypt.key);
                             else decryptedtext[i] = 'Z' - (decrypt.key - (encryptedtext[i] % ('A')) - 1);
                         }
-                        
+
                     }
                     else
                         decryptedtext[i] = encryptedtext[i] - decrypt.key;
@@ -364,30 +337,43 @@ void Entschluesseln() {
                         endlines++;
                         decryptedtext[i] = encryptedtext[i];
                     }
-                    
+
                 }
-                
-                /*for (int i = 0; i < lowerEingabe.length(); i++) {
-                    if (lowerEingabe[i] > 'a' + schluessel - 1)
-                    {
-                        lowerEingabe[i] -= schluessel;
-                    }
-                    else lowerEingabe[i] = 'z' - (schluessel - (lowerEingabe[i] % ('a')) - 1);
-                }*/
+                DateiEnt.open(path + decrypt.DateinameEnt + ".txt");
 
-                decrypt.DateiEnt.open(path + decrypt.DateinameEnt + ".txt");
+                DateiEnt << decryptedtext;
 
-                decrypt.DateiEnt << decryptedtext;
-
-                decrypt.DateiEnt.close();
+                DateiEnt.close();
 
             }
             else {
-                Wahl = 0;
                 break;
             }
+        }
+        case 5: {
+
             break;
         }
         }
+
+    }
+}
+
+
+void Bruteforce() {
+    ifstream DateiLesen;
+    ofstream DateiSpeicher;
+
+    string Eingabe;
+    string TextDatei = "", line;
+    vector<int> posAlphabet;
+    vector<char> charAlphabet;
+
+    cout << "Welche der Folgenden Dateien moechten Sie entschluesseln?\n";
+    SHOWDIR;
+
+    while (!DateiLesen.is_open()) {
+        getline(cin, Eingabe);
+        DateiLesen.open(path + Eingabe + ".txt");
     }
 }
